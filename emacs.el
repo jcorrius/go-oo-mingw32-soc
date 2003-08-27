133bb2bb4ff195d81f2067bc43924ce525fcf00c
Martin Kretzschmar <m_kretzschmar@gmx.net>
2003-08-27 20:04:44 +0000

    Www/hackers-guide.
    www/hackers-guide.html (FAQ): "Why does the code look so ugly?"
    
    * emacs.el: elisp for nicer code display.

---
+++ emacs.el
@@ -0,0 +1,19 @@
+(defvar my-openoffice-path-regexp
+  "openoffice"
+  "Regexp that matches paths into your OpenOffice.org source tree.")
+
+(defun my-openoffice-c-hook ()
+  (when (string-match my-openoffice-path-regexp buffer-file-name)
+      (message "Using OpenOffice.org code settings")
+      (setq tab-width 4)
+      (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72
+			    76 80 84 88 92 96 100 104 108 112 116 120))
+      (c-set-style "stroustrup")
+      (set-variable 'c-basic-offset 4)
+      (set-variable 'indent-tabs-mode t)))
+
+(add-hook 'c-mode-common-hook 'my-openoffice-c-hook)
+
+(add-to-list 'auto-mode-alist (cons "\\.sdi\\'" 'c++-mode))
+(add-to-list 'auto-mode-alist (cons "\\.hrc\\'" 'c++-mode))
+(add-to-list 'auto-mode-alist (cons "\\.src\\'" 'c++-mode))
