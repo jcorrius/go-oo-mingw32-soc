9f2e22e188b5d45c675fa29cd0cffb45dbee5544
Michael Meeks <michael.meeks@novell.com>
2004-04-29 12:25:18 +0000

    Add '--clean' option.
    * autogen.sh: add '--clean' option.
    
    * Makefile.am (EXTRA_DIST): don't try to
    distribute generated mkinstalldirs file.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+if test "z$1" == "z--clean"; then
+    echo "Cleaning"
+
+    rm -Rf autom4te.cache bonobo/autom4te.cache
+    rm -f missing install-sh mkinstalldirs libtool ltmain.sh
+    rm -f bonobo/missing bonobo/install-sh bonobo/mkinstalldirs \
+	bonobo/libtool bonobo/ltmain.sh
+    exit 1;
+fi
+
 (cd bonobo && ./autogen.sh)
 echo "NOT YET."
 
