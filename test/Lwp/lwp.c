c3723a7713cd4054efc0d9e5446b54cc2c6603d8
Michael Meeks <michael.meeks@novell.com>
2004-09-20 10:54:56 +0000

    Upd.

--- test/Lwp/lwp.c
+++ test/Lwp/lwp.c
@@ -15,19 +15,35 @@ print_special (unsigned char c)
 }
 
 static void
-dump_string (unsigned char *data, int len)
+dump_string (FILE *fin, int char_len)
 {
-		int i;
-		print_special (data[0]);
-		for (i = 1; i < len; i++) {
-				if (isascii (data[i]))
-						fprintf (stderr, "%c", data[i]);
-				else switch (data [i]) {
+		byte_t datum;
+		while (char_len > 0) {
+				datum = fgetc(fin);
+				if (isascii (datum)) {
+						fprintf (stderr, "%c", datum);
+						char_len--;
+				} else switch (datum) {
+				case 0x91:
+				case 0x88:
+				case 0x8f:
 				case 0xc6:
-						fprintf (stderr, "<word-break>");
+						fprintf (stderr, "<magicnolen0x%x>", datum);
+						break;
+				case 0x82:
+				case 0x8a:	
+				case 0x8d:
+				case 0x97:
+				case 0xa9:
+				case 0xb9:
+				case 0xbd:
+				case 0xbf:
+						char_len--;
+						fprintf (stderr, "<magic0x%x>", datum);
 						break;
 				default:
-						print_special (data[i]);
+						fprintf (stderr, "Error: unknown code 0x%x\n", datum);
+						print_special (datum);
 						break;
 				}
 		}
@@ -70,7 +86,7 @@ get_len (byte_t codea, byte_t codeb, ValueType *type)
 		if (codea == 0x01) {
 				switch (codeb) {
 				MAP( 0x02, 8 ); /* SSEnd */
-				MAP( 0x41, 0 ); MAP( 0x42, 0 );
+				MAP( 0x40, 0); MAP( 0x41, 0 ); MAP( 0x42, 0 );
 				MAP( 0x6d, 4 );
 				MAP( 0x96, 1 );
 				MAP( 0xc0, 0 );
@@ -85,17 +101,27 @@ get_len (byte_t codea, byte_t codeb, ValueType *type)
 						len = (byte_t) fgetc(in) + 1;
 						*type = VALUE_STRING;
 						break;
+				case 0x04: 
+				{ /* Almost certainly wrong */
+						int i;
+						for (i = 0; i < 10; i++) fgetc(in);
+						len = (byte_t) fgetc(in) + 1;
+						*type = VALUE_STRING;
+						break;
+				}
 				case 0x20:
 				case 0x26: case 0x27:
 				case 0x28: case 0x29: case 0x2a: case 0x2b:
 				case 0x2c: case 0x2d: case 0x2e: case 0x2f:
+				case 0x30: case 0x31: case 0x32:
 						len = 1;
 						break;
 				MAP( 0x53, 7 ); /* SSEnd */
 				case 0x5c:
-				case 0x60:
+				case 0x60: case 0x62:
 				case 0x64:
 				case 0x6a:
+				case 0x76:
 						len = 1;
 						break;
 				default:
@@ -104,19 +130,23 @@ get_len (byte_t codea, byte_t codeb, ValueType *type)
 						break;
 				}
 		}
-		if (codea == 0x08 || codea == 0x09 ||
-			codea == 0x0a || codea == 0x0b) {
-				switch (codeb) {
-				case 0x61:
-						len = 0;
-						break;
-				default:
-						break;
-				}
-		}
+		if (codea == 0x20 && codeb == 0x40)
+				len = 0;
+
+		if (codea >= 0x08 && codea <= 0x11 &&
+			codeb == 0x61)
+				len = 0;
 		if (len < 0 && codeb == 0x61) {
 				fprintf( stderr, "Guess (B) length 0 for 0x%x\n", codea );
 				len = 0;
+		}
+
+		if ((codea == 0x12 || codea == 0x13) &&
+			codeb == 0x50)
+				len = 2;
+		if (len < 0 && codeb == 0x50) {
+				fprintf( stderr, "Guess (C) length 2 for 0x%x\n", codea );
+				len = 2;
 		}
 #undef MAP
 		return len;
@@ -141,11 +171,10 @@ main (int argc, char **argv)
 						len = 1024;
 						die = 1;
 				}
-				fread (data, 1, len, in);
 				if (type == VALUE_STRING)
 				{
-						fprintf (stderr, "String: %.2x%.2x: ", codea, codeb);
-						dump_string (data, len);
+						fprintf (stderr, "String (0x%x): %.2x%.2x: ", len, codea, codeb);
+						dump_string (in, len);
 				}
 				else
 				{
@@ -153,6 +182,7 @@ main (int argc, char **argv)
 								fprintf (stderr, "Flag: 0x%.2x%.2x\n", codea, codeb);
 						else
 						{
+								fread (data, 1, len, in);
 								fprintf (stderr, "Record: 0x%.2x%.2x (0x%d bytes): ",
 										 codea, codeb, len );
 								dump_hex (data, len);
