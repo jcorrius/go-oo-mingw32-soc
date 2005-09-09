a378d894a659a61107928b7f9059c8e00ef186b7
Jayant Madavi <mjayant@novell.com>
2005-09-09 13:58:46 +0000

    scratch/sc-vba/vbainterior.[hc]xx: Modified the files to implement XInvocation for two test properties.

--- scratch/sc-vba/vbainterior.cxx
+++ scratch/sc-vba/vbainterior.cxx
@@ -1,6 +1,12 @@
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/table/XCell.hpp>
 #include <com/sun/star/table/XColumnRowRange.hpp>
+#include <com/sun/star/beans/XIntrospection.hpp>
+#include <com/sun/star/beans/XIntrospectionAccess.hpp>
+#include <com/sun/star/reflection/XIdlMethod.hpp>
+#include <com/sun/star/beans/MethodConcept.hpp>
+#include <comphelper/processfactory.hxx>
+#include <cppuhelper/queryinterface.hxx>
 
 #include "vbainterior.hxx"
 
@@ -10,7 +16,7 @@ ScVbaInterior::getColor() throw (::com::sun::star::uno::RuntimeException)
 	uno::Any aAny;
 	uno::Reference< table::XCell > xCell = mxRange->getCellByPosition(0,0 );
 	uno::Reference< beans::XPropertySet > xProps(xCell, uno::UNO_QUERY );
-        aAny = xProps->getPropertyValue(rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "CellBackColor" ) ));
+	aAny = xProps->getPropertyValue(rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "CellBackColor" ) ));
 	return aAny;
 }
  
@@ -21,3 +27,66 @@ ScVbaInterior::setColor( const ::com::sun::star::uno::Any& _color ) throw (::com
 	xProps->setPropertyValue(rtl::OUString( RTL_CONSTASCII_USTRINGPARAM( "CellBackColor" ) ), _color);
 }
 
+Reference< XIntrospectionAccess >
+ScVbaInterior::getIntrospection(void)  throw(RuntimeException)
+{
+	uno::Any aAny;
+	uno::Reference<beans::XIntrospectionAccess> xAcc;
+	uno::Reference< beans::XIntrospection> xIspn(::comphelper::getProcessServiceFactory()->createInstance(::rtl::OUString::createFromAscii("com.sun.star.beans.Introspection")), uno::UNO_QUERY);
+	uno::Reference< beans::XPropertySet > xProps(mxRange, uno::UNO_QUERY );
+
+	if (xIspn.is() &&  xProps.is())
+	{
+		aAny <<= xProps;
+		xAcc = xIspn->inspect(aAny);
+	}
+
+	return xAcc;
+}
+
+Any
+ScVbaInterior::invoke(const OUString& FunctionName, const Sequence< Any >& Params, Sequence< sal_Int16 >& OutParamIndex, Sequence< Any >& OutParam) throw(IllegalArgumentException, CannotConvertException, InvocationTargetException, RuntimeException)
+{
+	uno::Any aAny;
+	return aAny;
+}
+
+void
+ScVbaInterior::setValue(const OUString& PropertyName, const Any& Value) throw(UnknownPropertyException, CannotConvertException, InvocationTargetException, RuntimeException)
+{
+	uno::Reference< beans::XPropertySet > xProps(mxRange, uno::UNO_QUERY );
+
+	if (PropertyName.equalsAscii("RotateAngle")) 
+	{
+		xProps->setPropertyValue(PropertyName,Value);
+	}
+	if (PropertyName.equalsAscii("CellStyle")) 
+	{
+		xProps->setPropertyValue(PropertyName,Value);
+	}
+}
+
+Any
+ScVbaInterior::getValue(const OUString& PropertyName) throw(UnknownPropertyException, RuntimeException)
+{
+	uno::Any aAny;
+	return aAny;
+}
+
+sal_Bool
+ScVbaInterior::hasMethod(const OUString& Name)  throw(RuntimeException)
+{
+	return false;
+}
+
+sal_Bool
+ScVbaInterior::hasProperty(const OUString& Name)  throw(RuntimeException)
+{
+
+	if (Name.equalsAscii("RotateAngle"))
+		return true;
+	if (Name.equalsAscii("CellStyle"))
+		return true;
+
+ return false;
+}
