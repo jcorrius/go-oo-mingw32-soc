c21eb405e4fde923631671638ed1e662d1add335
Kohei Yoshida <kyoshida@novell.com>
2006-08-03 02:02:12 +0000

    Moved method definitions into class declaration, plus some code cleanup.
    * source/numeric/nlpqnewton.cxx: moved method definitions into class declaration,
    plus some code cleanup.

--- scratch/scsolver/source/numeric/nlpqnewton.cxx
+++ scratch/scsolver/source/numeric/nlpqnewton.cxx
@@ -25,12 +25,13 @@
  *
  ************************************************************************/
 
-#include <numeric/nlpnewton.hxx>
-#include <numeric/diff.hxx>
-#include <numeric/nlpmodel.hxx>
-#include <numeric/funcobj.hxx>
+#include "numeric/nlpnewton.hxx"
+#include "numeric/diff.hxx"
+#include "numeric/nlpmodel.hxx"
+#include "numeric/funcobj.hxx"
+#include "global.hxx"
+
 #include <boost/shared_ptr.hpp>
-#include <global.hxx>
 #include <memory>
 #include <iostream>
 #include <iomanip>
@@ -43,20 +44,104 @@ using namespace std;
 
 namespace scsolver { namespace numeric { namespace opres { namespace nlp {
 
-//---------------------------------------------------------------------------
-// IMPLEMENTATION
-
 class QuasiNewtonImpl
 {
-	static double norm( const Matrix& );
-	static double evalF( const BaseFuncObj&, const Matrix&, vector<double>& );
-	static double evalF( const BaseFuncObj&, const Matrix& );
+	/**
+     * Calculate the norm of a set of variables.
+     * 
+     * @param mxX one-column matrix representing delta f(x) vector
+     * 
+     * @return double calculated norm
+	 */
+	static double norm( const Matrix& mxX )
+	{
+		double fNorm = 0.0;
+		for ( size_t i = 0; i < mxX.rows(); ++i )
+		{
+			double f = mxX( i, 0 );
+			fNorm += f*f;
+		}
+#ifdef _MSC_VER
+		return ::sqrt( fNorm );
+#else
+		return std::sqrt( fNorm );
+#endif
+	}
+
+	static double evalF( const BaseFuncObj& oF, const Matrix& mxVars, vector<double>& fVars )
+	{
+		size_t nRows = mxVars.rows();
+		fVars.reserve( nRows );
+		for ( size_t i = 0; i < nRows; ++i )
+			fVars.push_back( mxVars( i, 0 ) );
+
+		return oF.eval( fVars );
+	}
+
+	static double evalF( const BaseFuncObj& oF, const Matrix& mxVars )
+	{
+		vector<double> fVars;
+		return evalF( oF, mxVars, fVars );
+	}
 
 public:
-	QuasiNewtonImpl( QuasiNewton* );
+	QuasiNewtonImpl( QuasiNewton* p ) :
+		m_pSelf(p),
+		m_pDiff(new Differentiate()),
+		m_nIter(0),
+		m_pModel(NULL),
+		m_mxVars(0, 0),
+		m_mxdVars(0, 0),
+		m_mxVarsOld(0, 0),
+		m_mxdF(0, 0),
+		m_mxdFOld(0, 0),
+		m_mxD(0, 0),
+		m_mxDOld(0, 0),
+		m_fF(0.0),
+		m_fFOld(0.0),
+		m_fNorm(0.0),
+		m_fTolerance(0.0)
+	{
+	}
+
 	~QuasiNewtonImpl() throw() {}
 
-	void solve();
+	void solve()
+	{
+		// Initialize relevant data members.
+		m_pModel = m_pSelf->getModel();
+		//vector<double> cnVars = m_pModel->getVariable();
+		vector<double> cnVars = m_pModel->getVars();
+
+		vector<double>::const_iterator it, itBeg = cnVars.begin(), itEnd = cnVars.end();
+		m_mxVars.clear();
+		for ( it = itBeg; it != itEnd; ++it )
+			m_mxVars( distance( itBeg, it ), 0 ) = *it;
+
+		m_mxdVars.clear();
+		m_mxVarsOld.clear();
+		m_mxdF.clear();
+		m_mxdFOld.clear();
+		m_mxD.clear();
+		m_mxDOld.clear();
+		m_fTolerance = 0.1;
+		for ( unsigned long i = 0; i < m_pModel->getPrecision(); ++i )
+			m_fTolerance *= 0.1;
+
+		m_fF = 0.0;
+		m_fFOld = m_fTolerance * 100.0;
+		m_fNorm = m_fTolerance * 100.0;
+
+		m_pFuncObj = m_pModel->getFuncObject();
+		m_pDiff->setFuncObject( m_pFuncObj );
+
+		m_nIter = 0;
+		bool bIterDone = false;
+		cout << setprecision( m_pModel->getPrecision() );
+		while ( !runIteration() );
+
+		cout << "f(x) = " << m_fF << endl;
+	}
 
 private:
 	QuasiNewton* m_pSelf;
@@ -79,277 +164,182 @@ private:
 
 	boost::shared_ptr<BaseFuncObj> m_pFuncObj;
 
-	bool evaluateFunc();
-	bool calcDefMatrix();
-	double runLinearSearch();
-	bool runIteration();
-};
-
-/** Calculate the norm of an array of variables.
-
-	@param mxX one-column matrix representing delta f(x) vector
-
-	@return calculate norm
- */
-double QuasiNewtonImpl::norm( const Matrix& mxX )
-{
-	double fNorm = 0.0;
-	for ( size_t i = 0; i < mxX.rows(); ++i )
+	bool evaluateFunc()
 	{
-		double f = mxX( i, 0 );
-		fNorm += f*f;
-	}
-#ifdef _MSC_VER
-	return ::sqrt( fNorm );
-#else
-	return std::sqrt( fNorm );
-#endif
-}
+		using namespace std;
 
-double QuasiNewtonImpl::evalF( const BaseFuncObj& oF, const Matrix& mxVars, vector<double>& fVars )
-{
-	size_t nRows = mxVars.rows();
-	fVars.reserve( nRows );
-	for ( size_t i = 0; i < nRows; ++i )
-		fVars.push_back( mxVars( i, 0 ) );
+		// Solve f(x) given the x vector
 
-	return oF.eval( fVars );
-}
+		vector<double> fVars;
+		m_fF = QuasiNewtonImpl::evalF( *m_pFuncObj, m_mxVars, fVars );
+		size_t nRows = fVars.size();
 
-double QuasiNewtonImpl::evalF( const BaseFuncObj& oF, const Matrix& mxVars )
-{
-	vector<double> fVars;
-	return evalF( oF, mxVars, fVars );
-}
+		// Calculate df(x) gradient array
+		m_pDiff->setVariables( fVars );
+		for ( size_t i = 0; i < nRows; ++i )
+		{
+			m_pDiff->setVarIndex( i );
+			m_mxdF( i, 0 ) = m_pDiff->run();
+		}
+		m_mxdF.print();
+		m_fNorm = QuasiNewtonImpl::norm( m_mxdF );
 
-QuasiNewtonImpl::QuasiNewtonImpl( QuasiNewton* p ) :
-	m_pSelf( p ),
-	m_pDiff( new Differentiate() ),
-	m_nIter( 0 ),
-	m_pModel( NULL ),
-	m_mxVars( 0, 0 ),
-	m_mxdVars( 0, 0 ),
-	m_mxVarsOld( 0, 0 ),
-	m_mxdF( 0, 0 ),
-	m_mxdFOld( 0, 0 ),
-	m_mxD( 0, 0 ),
-	m_mxDOld( 0, 0 ),
-	m_fF( 0.0 ),
-	m_fFOld( 0.0 ),
-	m_fNorm( 0.0 ),
-	m_fTolerance( 0.0 )
-{
-}
-
-void QuasiNewtonImpl::solve()
-{
-	// Initialize relevant data members.
-	m_pModel = m_pSelf->getModel();
-	//vector<double> cnVars = m_pModel->getVariable();
-	vector<double> cnVars = m_pModel->getVars();
-	
-	vector<double>::const_iterator it, itBeg = cnVars.begin(), itEnd = cnVars.end();
-	m_mxVars.clear();
-	for ( it = itBeg; it != itEnd; ++it )
-		m_mxVars( distance( itBeg, it ), 0 ) = *it;
-
-	m_mxdVars.clear();
-	m_mxVarsOld.clear();
-	m_mxdF.clear();
-	m_mxdFOld.clear();
-	m_mxD.clear();
-	m_mxDOld.clear();
-	m_fTolerance = 0.1;
-	for ( unsigned long i = 0; i < m_pModel->getPrecision(); ++i )
-		m_fTolerance *= 0.1;
-
-	m_fF = 0.0;
-	m_fFOld = m_fTolerance * 100.0;
-	m_fNorm = m_fTolerance * 100.0;
-
-	m_pFuncObj = m_pModel->getFuncObject();
-	m_pDiff->setFuncObject( m_pFuncObj );
-
-	m_nIter = 0;
-	bool bIterDone = false;
-	cout << setprecision( m_pModel->getPrecision() );
-	while ( !runIteration() );
-
-	cout << "f(x) = " << m_fF << endl;
-}
-
-bool QuasiNewtonImpl::evaluateFunc()
-{
-	using namespace std;
-
-	// Solve f(x) given the x vector
-
-	vector<double> fVars;
-	m_fF = QuasiNewtonImpl::evalF( *m_pFuncObj, m_mxVars, fVars );
-	size_t nRows = fVars.size();
-
-	// Calculate df(x) gradient array
-	m_pDiff->setVariables( fVars );
-	for ( size_t i = 0; i < nRows; ++i )
-	{
-		m_pDiff->setVarIndex( i );
-		m_mxdF( i, 0 ) = m_pDiff->run();
-	}
-	m_mxdF.print();
-	m_fNorm = QuasiNewtonImpl::norm( m_mxdF );
-
-	if ( fabs( m_fF - m_fFOld ) < m_fTolerance )
-	{
-		if ( m_pModel->getVerbose() )
-			cout << "desired precision has been reached" << endl;
-		return true;
-	}
-
-	if ( m_fNorm < m_fTolerance )
-	{
-		if ( m_pModel->getVerbose() )
-			cout << "norm is below specified tolerance limit" << endl;
-		return true;
-	}
-
-	return false;
-}
-
-/** Calculate or initialize deflection matrix (D).
-
-	@return true if the iteration should stop, or false otherwise
- */
-bool QuasiNewtonImpl::calcDefMatrix()
-{
-	if ( m_mxD.empty() )
-	{
-		// Deflection matrix is empty.  Initialize it.
-		size_t nX = m_mxVars.rows();
-		Matrix mxTemp( nX, nX, true );
-		m_mxD.swap( mxTemp );
-		if ( m_pModel->getGoal() == GOAL_MAXIMIZE )
-			m_mxD *= -1.0;
-	}
-	else
-	{
-		// Both mxD and mxG are columnar matrix.
-		Matrix mxD = m_mxVars - m_mxVarsOld;
-		Matrix mxG = m_mxdF - m_mxdFOld;
-		double fDG = ( mxD.trans()*mxG )( 0, 0 );
-		if ( fDG == 0.0 )
+		if ( fabs( m_fF - m_fFOld ) < m_fTolerance )
 		{
 			if ( m_pModel->getVerbose() )
-				cout << "dg is zero!" << endl;
+				cout << "desired precision has been reached" << endl;
 			return true;
 		}
 
-		// Calculate new deflection matrix via BFGS formula
-		Matrix mxA = mxG.trans() * m_mxDOld * mxG;
-		double fA = 1.0 + mxA( 0, 0 ) / fDG;
-		Matrix mxB = mxD * mxD.trans() / fDG;
-		Matrix mxC = ( m_mxDOld * mxG * mxD.trans() + mxD * mxG.trans() * m_mxDOld ) / fDG;
-		m_mxD = m_mxDOld + mxB*fA - mxC;
+		if ( m_fNorm < m_fTolerance )
+		{
+			if ( m_pModel->getVerbose() )
+				cout << "norm is below specified tolerance limit" << endl;
+			return true;
+		}
+
+		return false;
 	}
 
-	m_mxdVars = m_mxD * m_mxdF*(-1.0);
-
-	if ( m_pModel->getVerbose() )
+	/**
+     * Calculate or initialize deflection matrix (D).
+	 * 
+     * @return bool true if the iteration should stop, or false
+     *         otherwise.
+	 */
+	bool calcDefMatrix()
 	{
-		unsigned long nPrec = m_pModel->getPrecision();
-		cout << setprecision( nPrec );
-		cout << "f(x) = " << m_fF << endl;
-		cout << "df(x) = ";
-		m_mxdF.trans().print( nPrec );
-		cout << "|| df(x) || = " << m_fNorm << endl;
-		cout << "D:";
-		m_mxD.print( nPrec );
-		cout << "dx = ";
-		m_mxdVars.trans().print( nPrec );
-	}
-
-	return false;
-}
-
-/** One-dimensional search (needs improvement)
- */
-double QuasiNewtonImpl::runLinearSearch()
-{
-	// m_mxVars  : Original X
-	// m_mxdVars : dX
-	// m_fF      : Original f(x)
-
-	double fInterval = 0.1;
-	double fLambda = 0.0;
-// 	double fTolerance = m_fTolerance;
-
-	Matrix mxVarsOriginal = m_mxVars.clone();
-
-	double fF = m_fF;
-
-	while ( true )
-	{
-		// Inspect two points
-		Matrix mxVars = mxVarsOriginal + m_mxdVars * ( fLambda + fInterval );
-		double fF1 = QuasiNewtonImpl::evalF( *m_pFuncObj, mxVars );
-		mxVars = mxVarsOriginal + m_mxdVars * ( fLambda + fInterval/2.0 );
-		double fF2 = QuasiNewtonImpl::evalF( *m_pFuncObj, mxVars );
-
-		double fSign = m_pModel->getGoal() == GOAL_MAXIMIZE ? 1.0 : -1.0;
-		if ( fF2*fSign >= fF*fSign )
-			if ( fF1*fSign >= fF2*fSign )
-			{
-				fLambda += fInterval;
-				fF = fF1;
-			}
-			else
-			{
-				fLambda += fInterval/2.0;
-				return fLambda;
-			}
+		if ( m_mxD.empty() )
+		{
+			// Deflection matrix is empty.  Initialize it.
+			size_t nX = m_mxVars.rows();
+			Matrix mxTemp( nX, nX, true );
+			m_mxD.swap( mxTemp );
+			if ( m_pModel->getGoal() == GOAL_MAXIMIZE )
+				m_mxD *= -1.0;
+		}
 		else
-			fInterval /= 2.0;
+		{
+			// Both mxD and mxG are columnar matrix.
+			Matrix mxD = m_mxVars - m_mxVarsOld;
+			Matrix mxG = m_mxdF - m_mxdFOld;
+			double fDG = ( mxD.trans()*mxG )( 0, 0 );
+			if ( fDG == 0.0 )
+			{
+				if ( m_pModel->getVerbose() )
+					cout << "dg is zero!" << endl;
+				return true;
+			}
+
+			// Calculate new deflection matrix via BFGS formula
+			Matrix mxA = mxG.trans() * m_mxDOld * mxG;
+			double fA = 1.0 + mxA( 0, 0 ) / fDG;
+			Matrix mxB = mxD * mxD.trans() / fDG;
+			Matrix mxC = ( m_mxDOld * mxG * mxD.trans() + mxD * mxG.trans() * m_mxDOld ) / fDG;
+			m_mxD = m_mxDOld + mxB*fA - mxC;
+		}
+
+		m_mxdVars = m_mxD * m_mxdF*(-1.0);
+
+		if ( m_pModel->getVerbose() )
+		{
+			unsigned long nPrec = m_pModel->getPrecision();
+			cout << setprecision( nPrec );
+			cout << "f(x) = " << m_fF << endl;
+			cout << "df(x) = ";
+			m_mxdF.trans().print( nPrec );
+			cout << "|| df(x) || = " << m_fNorm << endl;
+			cout << "D:";
+			m_mxD.print( nPrec );
+			cout << "dx = ";
+			m_mxdVars.trans().print( nPrec );
+		}
+
+		return false;
 	}
-	assert( !"logic error" );
-}
 
-bool QuasiNewtonImpl::runIteration()
-{
-	Debug( "runIteration" );
-
-	if ( m_pModel->getVerbose() )
+	/**
+     * One-dimensional search (needs improvement).
+	 * 
+	 * @return double
+	 */
+	double runLinearSearch()
 	{
-		cout << repeatString( "-", 70 ) << endl;
-		cout <<	"Iteration " << m_nIter << endl;
-		cout << repeatString( "-", 70 ) << endl;
-		cout << "x = ";
-		m_mxVars.print( m_pModel->getPrecision() );
+		// m_mxVars  : Original X
+		// m_mxdVars : dX
+		// m_fF      : Original f(x)
+
+		double fInterval = 0.1;
+		double fLambda = 0.0;
+	// 	double fTolerance = m_fTolerance;
+
+		Matrix mxVarsOriginal = m_mxVars.clone();
+
+		double fF = m_fF;
+
+		while ( true )
+		{
+			// Inspect two points
+			Matrix mxVars = mxVarsOriginal + m_mxdVars * ( fLambda + fInterval );
+			double fF1 = QuasiNewtonImpl::evalF( *m_pFuncObj, mxVars );
+			mxVars = mxVarsOriginal + m_mxdVars * ( fLambda + fInterval/2.0 );
+			double fF2 = QuasiNewtonImpl::evalF( *m_pFuncObj, mxVars );
+
+			double fSign = m_pModel->getGoal() == GOAL_MAXIMIZE ? 1.0 : -1.0;
+			if ( fF2*fSign >= fF*fSign )
+				if ( fF1*fSign >= fF2*fSign )
+				{
+					fLambda += fInterval;
+					fF = fF1;
+				}
+				else
+				{
+					fLambda += fInterval/2.0;
+					return fLambda;
+				}
+			else
+				fInterval /= 2.0;
+		}
+		assert( !"logic error" );
 	}
 
-	if ( evaluateFunc() )
-		return true;
+	bool runIteration()
+	{
+		Debug( "runIteration" );
 
-	if ( calcDefMatrix() )
-		return true;
+		if ( m_pModel->getVerbose() )
+		{
+			cout << repeatString( "-", 70 ) << endl;
+			cout <<	"Iteration " << m_nIter << endl;
+			cout << repeatString( "-", 70 ) << endl;
+			cout << "x = ";
+			m_mxVars.print( m_pModel->getPrecision() );
+		}
 
-	double fLambda = runLinearSearch();
-	if ( m_pModel->getVerbose() )
-		cout << "lambda = " << fLambda << endl;
+		if ( evaluateFunc() )
+			return true;
 
-	// Update parameters for next iteration
+		if ( calcDefMatrix() )
+			return true;
 
-	m_mxVarsOld = m_mxVars;
-	m_mxVars += m_mxdVars * fLambda;
-	m_mxdFOld = m_mxdF;
-	m_mxDOld = m_mxD;
-	m_fFOld = m_fF;
-	++m_nIter;
+		double fLambda = runLinearSearch();
+		if ( m_pModel->getVerbose() )
+			cout << "lambda = " << fLambda << endl;
 
-	return false;
-}
+		// Update parameters for next iteration
 
+		m_mxVarsOld = m_mxVars;
+		m_mxVars += m_mxdVars * fLambda;
+		m_mxdFOld = m_mxdF;
+		m_mxDOld = m_mxD;
+		m_fFOld = m_fF;
+		++m_nIter;
 
-//---------------------------------------------------------------------------
-// ENVELOPE
+		return false;
+	}
+};
+
+//-----------------------------------------------------------------
 
 QuasiNewton::QuasiNewton() : m_pImpl( new QuasiNewtonImpl( this ) )
 {
