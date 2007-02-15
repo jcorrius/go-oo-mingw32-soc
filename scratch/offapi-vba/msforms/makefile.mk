eee50c85fbfb26af6358e625e28ee953e1ef681f
Noel Power <noel.power@novell.com>
2007-02-15 13:32:41 +0000

    New namespace and supporting changes to the idl

---
+++ scratch/offapi-vba/msforms/makefile.mk
@@ -0,0 +1,30 @@
+PRJ=..$/..$/..
+
+PRJNAME=oovbapi
+
+TARGET=msforms
+PACKAGE=org$/openoffice$/msforms
+
+# --- Settings -----------------------------------------------------
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
+.IF "$(ENABLE_VBA)"!="YES"
+dummy:
+        @echo "not building vba..."
+.ENDIF
+
+# ------------------------------------------------------------------------
+
+IDLFILES=\
+	MSFormReturnTypes.idl \
+	XComboBox.idl \
+	XButton.idl \
+	XControl.idl \
+	XLabel.idl \
+	XTextBox.idl \
+	XRadioButton.idl \
+	XListBox.idl \
+
+# ------------------------------------------------------------------
+
+.INCLUDE :  target.mk
---
+++ scratch/offapi-vba/vba/makefile.mk
@@ -0,0 +1,29 @@
+PRJ=..$/..$/..
+
+PRJNAME=oovbapi
+
+TARGET=vba
+PACKAGE=org$/openoffice$/vba
+
+# --- Settings -----------------------------------------------------
+.INCLUDE :  $(PRJ)$/util$/makefile.pmk
+
+.IF "$(ENABLE_VBA)"!="YES"
+dummy:
+        @echo "not building vba..."
+.ENDIF
+
+# ------------------------------------------------------------------------
+
+IDLFILES=\
+	VbMsgBoxResult.idl \
+	VbMsgBoxStyle.idl \
+	XErrObject.idl \
+	XGlobals.idl \
+	XCollection.idl\
+	XVBAToOOEventDescGen.idl\
+	XPropValue.idl\
+
+# ------------------------------------------------------------------
+
+.INCLUDE :  target.mk
