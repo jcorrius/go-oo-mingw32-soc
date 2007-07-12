118178024b15527c17b26aac046fa08a60466788
Kohei Yoshida <kyoshida@novell.com>
2007-07-12 16:41:56 +0000

    Modified to  make them play nice with other patches.
    * patches/ooxml/common-scp2.diff:
    * patches/ooxml/writerfilter-postprocess.diff:
    * patches/ooxml/writerfilter-sw.diff:
    * patches/ooxml/xmlfilter-sc.diff:
    * patches/src680/apply:
    * patches/src680/lwp-filter-component.diff:
    * patches/src680/novell-win32-agfa-monotype-fonts.diff: modified to 
    make them play nice with other patches.
    
    * patches/ooxml/makelist.sh: updated script to make file list.

--- patches/ooxml/makelist.sh
+++ patches/ooxml/makelist.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-ls -l | grep -E "\.diff$" | sed -e "s/.*\ oox/oox/g" -e "s/.*\ xmlfilter/xmlfilter/g" > list.txt
+ls -l | grep -E "\.diff$" | sed \
+    -e "s/.*\ oox/oox/g" \
+    -e "s/.*\ xmlfilter/xmlfilter/g" \
+    -e "s/.*\ writerfilter/writerfilter/g" \
+    -e "s/.*\ ooxml/ooxml/g" \
+    | sort > list.txt
