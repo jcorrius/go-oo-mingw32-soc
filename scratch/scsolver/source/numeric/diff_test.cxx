58f69f59cfa6d5fa412da8e793e9f4aa9a9d8cc9
Kohei Yoshida <kyoshida@novell.com>
2007-07-16 03:24:27 +0000

    Beefed up the test code for numerical differentiation.
    * source/numeric/diff_test.cxx: beefed up the test code for numerical
    differentiation.

--- scratch/scsolver/source/numeric/diff_test.cxx
+++ scratch/scsolver/source/numeric/diff_test.cxx
@@ -30,12 +30,23 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <cmath>
 #include <memory>
 
 using namespace std;
 using namespace scsolver::numeric;
 
-class TestFunc : public BaseFuncObj
+class TestFailed {};
+
+class BaseTestFunc : public BaseFuncObj
+{
+public:
+    virtual ~BaseTestFunc() throw() {}
+    virtual double evalDef1() const = 0;
+    virtual double evalDef2() const = 0;
+};
+
+class TestFunc : public BaseTestFunc
 {
 public:
 
@@ -50,8 +61,7 @@ public:
 
     virtual const vector<double>& getVars() const
     {
-        static const vector<double> tmp;
-        return tmp;
+        return m_Vars;
     }
 
     virtual void setVars(const vector<double>& vars)
@@ -67,15 +77,26 @@ public:
 
     virtual double eval()
     {
-		double x = m_Vars.at( 0 );
-		return 2.0*x*x + 5.0*x;
+		double x = m_Vars.at(0);
+        return (2*x + 5)*x;
+    }
+
+    virtual double evalDef1() const
+    {
+        double x = m_Vars.at(0);
+        return 4*x + 5;
+    }
+
+    virtual double evalDef2() const
+    {
+        return 4;
     }
 
 private:
     vector<double> m_Vars;
 };
 
-class TestFunc2 : public BaseFuncObj
+class TestFunc2 : public BaseTestFunc
 {
 public:
     virtual ~TestFunc2() throw()
@@ -89,14 +110,29 @@ public:
 
 	virtual double eval()
 	{
+        // ( (2*x + 5) * x - 2)*x
 		double x = m_Vars.at( 0 );
-		return 2.0*x*x*x + 5.0*x*x - 2.0*x;
+        double f = ((2*x + 5)*x - 2)*x;
+        return f;
 	}
+
+    virtual double evalDef1() const
+    {
+        // 6x^2 + 10x - 2 = (6x + 10) * x - 2
+        double x = m_Vars.at(0);
+        double f = (6*x + 10) * x - 2;
+        return f;
+    }
+
+    virtual double evalDef2() const
+    {
+        double x = m_Vars.at(0);
+        return 12*x + 10;
+    }
 
     virtual const vector<double>& getVars() const
     {
-        static const vector<double> tmp;
-        return tmp;
+        return m_Vars;
     }
 
     virtual void setVars(const vector<double>& vars)
@@ -114,27 +150,52 @@ private:
     vector<double> m_Vars;
 };
 
-void test(BaseFuncObj* pF)
+void checkDelta(double delta)
 {
-	vector<double> cnX;
-	cnX.push_back( 1.0 );
-	auto_ptr<Differentiate> pDiff( new Differentiate );
-	pDiff->setVariables( cnX );
-	pDiff->setPrecision( 5 );
-	pDiff->setFuncObject( pF );
-	double fAns = pDiff->run();
-	cout << fAns << endl;
-	pDiff->setSecondOrder( true );
-	fAns = pDiff->run();
-	cout << fAns << endl;
+    cout << "delta = " << delta << endl;
+    if (delta > 0.05)
+        throw TestFailed();
+}
+
+void test(BaseTestFunc* pF)
+{
+    static const double vars[] = {0, 1.5, 10.0, 20.5, 50.0};
+    for (int i = 0; i < 5; ++i)
+    {
+        double var = vars[i];
+        cout << "----------------------------------------" << endl;
+        cout << " x = " << var << endl;
+        vector<double> cnX;
+        cnX.push_back(var);
+        auto_ptr<Differentiate> pDiff( new Differentiate );
+        pDiff->setVariables( cnX );
+        pDiff->setPrecision(5);
+        pDiff->setFuncObject(pF);
+        double answer = pDiff->run();
+        double delta = fabs(answer - pF->evalDef1())/fabs(answer);
+        checkDelta(delta);
+        
+        pDiff->setSecondOrder(true);
+        answer = pDiff->run();
+        delta = (answer - pF->evalDef2())/answer;
+        checkDelta(delta);
+    }
 }
 
 int main()
 {
-	auto_ptr<TestFunc> pF( new TestFunc );
-	test(pF.get());
-	auto_ptr<TestFunc2> pF2( new TestFunc2 );
-	test(pF2.get());
+    try
+    {
+        auto_ptr<TestFunc> pF( new TestFunc );
+        test(pF.get());
+        auto_ptr<TestFunc2> pF2( new TestFunc2 );
+        test(pF2.get());
+        cout << "Test passed!" << endl;
+    }
+    catch (const TestFailed& )
+    {
+        cout << "Test failed" << endl;
+    }
 }
 
 
