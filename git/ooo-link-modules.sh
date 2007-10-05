05009ffa447c2bae459c23eed8799868f6eb9498
Jan Holesovsky <kendy@suse.cz>
2007-10-05 07:57:38 +0000

    Dump parts of the git work here to have a backup.
    * git/*: Dump parts of the git work here to have a backup.

---
+++ git/ooo-link-modules.sh
@@ -0,0 +1,80 @@
+#! /bin/bash
+
+MODULESDIR="cvsup"
+MODULES="$MODULESDIR/CVSROOT/modules"
+LINKDIR="ooo"
+
+[ -d "$LINKDIR" ] && { echo "$LINKDIR already exists" ; exit 1 ; }
+
+# breaks a dir into a set of symlinks
+function linkdir() {
+    DIR="$1"
+    TMP=""
+    for I in `echo "$DIR" | sed 's#/# #g'`; do
+        if [ -z "$TMP" ] ; then
+            TMP="$I"
+        else
+            TMP="$TMP/$I"
+        fi
+        LINK=""
+        if [ -h "$TMP" ] ; then
+            LINK=`readlink "$TMP"`
+            rm "$TMP"
+            mkdir "$TMP"
+            (
+                cd "$TMP"
+                for J in ../"$LINK"/* ../"$LINK"/.[^.]* ; do
+                    [ -e "$J" ] && { ln -s "$J" . ; }
+                done
+            )
+        fi
+    done
+}
+
+ONLY="`grep '^OpenOffice2 -a' "$MODULES" | sed 's/^OpenOffice2 -a //'`"
+ONLY=" $ONLY "
+
+mkdir -p "$LINKDIR"
+mkdir "$LINKDIR/tmp"
+
+ln -s "../$MODULESDIR/CVSROOT" "$LINKDIR/CVSROOT"
+
+# link everything to openoffice.org subdir
+echo "Linking modules"
+SKIP=9
+while read A R ; do
+    if [ "$SKIP" -gt "0" ] ; then
+        SKIP=$(( $SKIP - 1 ))
+        continue
+    fi
+    [ "$ONLY" != "${ONLY/ $A /XYZ}" ] && ln -s "../../$MODULESDIR/$R" "$LINKDIR/tmp/$A"
+done < "$MODULES"
+ln -s "../../xml/oox" "$LINKDIR/tmp/oox"
+
+# move the modules according to ooo-link-modules.list
+grep -v "^#" ooo-link-modules.list | while read MOD ; do
+    D=${MOD%/*}
+    F=${MOD#*/}
+    [ -d "$LINKDIR/$D" ] || mkdir "$LINKDIR/$D"
+    mv "$LINKDIR/tmp/$F" "$LINKDIR/$MOD"
+done
+
+# anything remained?
+NOT_MOVED=`ls $LINKDIR/tmp/`
+[ -z "$NOT_MOVED" ] || echo "Not moved: $NOT_MOVED"
+
+# move the localization stuff modules to l10n subdir
+echo "Creating ooo-l10n"
+find -L "$LINKDIR" -name "localize.sdf,v" | while read F ; do
+    DIR="`dirname $F`"
+    TO="$LINKDIR/ooo-l10n/${DIR#*/*/}"
+
+    if [ "$DIR" != "$TO" ] ; then
+        mkdir -p "$TO"
+        linkdir "$DIR"
+        mv "$F" "$TO"/
+    fi
+done
+
+rmdir ooo/tmp
+echo "Done"
