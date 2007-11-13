022f3d27c9cfc4cedcd069cdef03d7bfdb4acccd
Kohei Yoshida <kyoshida@novell.com>
2007-11-13 20:10:34 +0000

    More test code for referenced items.  not finished yet.

--- scratch/sc-dptest/source/dptestbase.cxx
+++ scratch/sc-dptest/source/dptestbase.cxx
@@ -41,6 +41,7 @@
 #include <stdio.h>
 #include <cmath>
 #include <vector>
+#include <boost/shared_ptr.hpp>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -57,6 +58,7 @@ using ::com::sun::star::table::CellContentType;
 using ::rtl::OUString;
 using ::rtl::OUStringBuffer;
 using ::std::vector;
+using ::boost::shared_ptr;
 
 namespace dptest {
 
@@ -118,6 +120,18 @@ UnaryProc forEachCell(const CellRangeAddress& range, UnaryProc op)
 
 class ResultTester
 {
+    struct DataFieldSetting
+    {
+        sal_Int32 FieldId;
+        sheet::GeneralFunction Function;
+        shared_ptr<DataPilotFieldReference> FieldRef;
+
+        DataFieldSetting() : 
+            FieldRef(static_cast<DataPilotFieldReference*>(NULL)) 
+        {
+        }
+    };
+
 public:
     ResultTester(const RuntimeData& data, const Reference<XDataPilotTable2>& xDPTab) :
         maData(data), mxDPTab(xDPTab), mnFailureCount(0)
@@ -126,8 +140,9 @@ public:
     }
 
     ResultTester(const ResultTester& other) :
-        maData(other.maData), mxDPTab(other.mxDPTab), maDataFieldIds(other.maDataFieldIds), 
-        maFuncs(other.maFuncs), mnFailureCount(other.mnFailureCount)
+        maData(other.maData), mxDPTab(other.mxDPTab), 
+        maDataFieldSettings(other.maDataFieldSettings),
+        mnFailureCount(other.mnFailureCount)
     {
         init();
     }
@@ -138,23 +153,30 @@ public:
 
         Reference<container::XIndexAccess> xDataFields = xDPDesc->getDataFields();
         sal_Int32 fieldCount = xDataFields->getCount();
-        maFuncs.reserve(fieldCount);
-        maDataFieldIds.reserve(fieldCount);
+        maDataFieldSettings.reserve(fieldCount);
         for (sal_Int32 i = 0; i < fieldCount; ++i)
         {
             Reference<XDataPilotField> xField(xDataFields->getByIndex(i), UNO_QUERY_THROW);
+            DataFieldSetting setting;
 
             // Get the field ID of a given data field.
             Reference<container::XNamed> xNamed(xField, UNO_QUERY_THROW);
             OUString name = xNamed->getName();
-            maDataFieldIds.push_back(maData.CacheTable.getFieldIndex(name));
+            setting.FieldId = maData.CacheTable.getFieldIndex(name);
 
             // Get the function used for aggregation.
             Reference<XPropertySet> xPS(xField, UNO_QUERY_THROW);
-            Any any = xPS->getPropertyValue(ascii("Function"));
-            sheet::GeneralFunction func;
-            any >>= func;
-            maFuncs.push_back(func);
+            getPropertyValue(xPS, ascii("Function"), setting.Function);
+
+            // Get the referenced item information (if any).
+            bool hasReference = false;
+            getPropertyValue(xPS, ascii("HasReference"), hasReference);
+            if (hasReference)
+            {
+                setting.FieldRef.reset(new DataPilotFieldReference);
+                getPropertyValue(xPS, ascii("Reference"), *setting.FieldRef);
+            }
+            maDataFieldSettings.push_back(setting);
         }
     }
 
@@ -168,22 +190,12 @@ public:
         if (!(posData.PositionData >>= resData))
             return;
 
-        Reference<XCell> xCell = maData.OutputSheetRef->getCellByPosition(cell.Column, cell.Row);
-
-        if (xCell->getType() != table::CellContentType_VALUE)
-            return;
-
-
         vector<DataTable::Filter> filters;
         sal_Int32 filterSize = resData.FieldFilters.getLength();
         filters.reserve(filterSize);
         for (sal_Int32 i = 0; i < filterSize; ++i)
         {
             sal_Int32 nFieldId = maData.CacheTable.getFieldIndex(resData.FieldFilters[i].FieldName);
-//          fprintf(stdout, "ResultTester::(): field name = '%s' (%ld)  match value = '%s'\n",
-//                  OUStringToOString(resData.FieldFilters[i].FieldName, RTL_TEXTENCODING_UTF8).getStr(),
-//                  nFieldId,
-//                  OUStringToOString(resData.FieldFilters[i].MatchValue, RTL_TEXTENCODING_UTF8).getStr());fflush(stdout);
 
             if (nFieldId >= 0)
             {
@@ -194,19 +206,40 @@ public:
             }
         }
 
-        sal_Int32 nId = resData.DataFieldIndex;
-        const sheet::GeneralFunction func = maFuncs.at(nId);
-        const sal_Int32 nFieldId = maDataFieldIds.at(nId);
+        // ID only for the data field set, not the actual column ID.
+        sal_Int32 nId = resData.DataFieldIndex; 
+        const DataFieldSetting& setting = maDataFieldSettings.at(nId);
 
-        double val1 = xCell->getValue();
-        double val2 = maData.CacheTable.aggregateValue(filters, nFieldId, func);
-        if (val1 != val2)
+        if (setting.FieldRef.get())
         {
-            fprintf(stdout, "* VALUES DIFFER (%ld, %ld) : real value = %g  check value = %g (%s)\n", 
-                    cell.Row, cell.Column,
-                    val1, val2, getFunctionName(func).c_str());
-            fflush(stdout);
-            ++mnFailureCount;
+            // referenced item exists.
+            verifyRefValue(cell, setting, filters);
+        }
+        else
+        {
+            // normal display mode with no reference
+
+            Reference<XCell> xCell = maData.OutputSheetRef->getCellByPosition(cell.Column, cell.Row);
+            if (xCell->getType() != table::CellContentType_VALUE)
+            {
+                fprintf(stdout, "* CELL NOT VALUE (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+                return;
+            }
+
+            // This ID is the actual column ID of the data field.
+            const sal_Int32 nFieldId = setting.FieldId;
+            const sheet::GeneralFunction func = setting.Function;
+            double val1 = xCell->getValue();
+            double val2 = maData.CacheTable.aggregateValue(filters, nFieldId, func);
+            if (val1 != val2)
+            {
+                fprintf(stdout, "* VALUES DIFFER (%ld, %ld) : real value = %g  check value = %g (%s)\n", 
+                        cell.Row, cell.Column,
+                        val1, val2, getFunctionName(func).c_str());
+                fflush(stdout);
+                ++mnFailureCount;
+            }
         }
     }
 
@@ -215,12 +248,152 @@ public:
         return mnFailureCount;
     }
 
+    void verifyRefValue(const CellAddress& cell, const DataFieldSetting& setting, 
+                        const vector<DataTable::Filter>& filters)
+    {
+        Reference<XCell> xCell = maData.OutputSheetRef->getCellByPosition(cell.Column, cell.Row);
+        const sal_Int32 nFieldId = setting.FieldId;
+        const sheet::GeneralFunction func = setting.Function;
+        const DataPilotFieldReference& ref = *setting.FieldRef;
+        sal_Int32 refFieldId = maData.CacheTable.getFieldIndex(ref.ReferenceField);
+
+        // Obtain the aggregate value with the original filter set.
+        double valOrig = maData.CacheTable.aggregateValue(filters, nFieldId, func);
+
+        // Go through the filters and find the field that matches the referenced field, then
+        // replace the match value with the referenced item name.
+        vector<DataTable::Filter> filters2;
+        vector<DataTable::Filter>::const_iterator itr = filters.begin(), itrEnd = filters.end();
+        bool refItem = false;
+        for (; itr != itrEnd; ++itr)
+        {
+            if (itr->FieldIndex == refFieldId)
+            {
+                // This is the referenced field.  Replace the match value with
+                // the referenced item name.
+                DataTable::Filter filter(*itr);
+                sal_Int32 newStrId = DataTable::getStringId(ref.ReferenceItemName); 
+                if (filter.MatchStrId == newStrId)
+                    refItem = true;
+                else
+                    filter.MatchStrId = newStrId;
+                filters2.push_back(filter);
+            }
+            else
+                filters2.push_back(*itr);
+        }
+
+        table::CellContentType cellType = xCell->getType();
+        if (refItem)
+        {
+            if (cellType != table::CellContentType_EMPTY)
+            {
+                fprintf(stdout, "* CELL NOT EMPTY FOR REFERENCED ITEM (%ld, %ld)\n",
+                        cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            return;
+        }
+
+        if (cellType != table::CellContentType_VALUE)
+        {
+            fprintf(stdout, "* CELL DOES NOT CONTAIN VALUE (%ld, %ld)\n",
+                    cell.Row, cell.Column);
+            ++mnFailureCount;
+            return;
+        }
+
+        // Get the referenced value with the new filter set.
+        double valRef = maData.CacheTable.aggregateValue(filters2, nFieldId, func);
+
+        double valCell = xCell->getValue();
+
+        switch (ref.ReferenceType)
+        {
+            case DataPilotFieldReferenceType::NONE:
+                // no reference mode.
+                return;
+            case DataPilotFieldReferenceType::ITEM_DIFFERENCE:
+            {
+                // subtract the reference value and display the difference.
+                if (valCell != valOrig - valRef)
+                {
+                    fprintf(stdout, "* VALUES DIFFER (%ld, %ld) : real value = %g  check value %g - %g = %g (%s)\n",
+                            cell.Row, cell.Column,
+                            valCell, valOrig, valRef, valOrig - valRef,
+                            getFunctionName(func).c_str());
+                    ++mnFailureCount;
+                }
+            }
+            break;
+            case DataPilotFieldReferenceType::ITEM_PERCENTAGE:
+            {
+                // each result is dividied by its reference value.
+                fprintf(stdout, "* TEST CODE NOT IMPLEMENTED (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            break;
+            case DataPilotFieldReferenceType::ITEM_PERCENTAGE_DIFFERENCE:
+            {
+                // from each result, its reference value is subtracted, and the difference
+                // is further divided by the reference value.
+                fprintf(stdout, "* TEST CODE NOT IMPLEMENTED (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            break;
+            case DataPilotFieldReferenceType::RUNNING_TOTAL:
+            {
+                // Each result is added to the sum of the results for preceding items 
+                // in the base field, in the base field's sort order, and the total 
+                // sum is shown.
+                fprintf(stdout, "* TEST CODE NOT IMPLEMENTED (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            break;
+            case DataPilotFieldReferenceType::ROW_PERCENTAGE:
+            {
+                // Each result is divided by the total result for its row in 
+                // the DataPilot table. 
+                fprintf(stdout, "* TEST CODE NOT IMPLEMENTED (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            break;
+            case DataPilotFieldReferenceType::COLUMN_PERCENTAGE:
+            {
+                // Same as DataPilotFieldReferenceType::ROW_PERCENTAGE , but the total 
+                // for the result's column is used. 
+                fprintf(stdout, "* TEST CODE NOT IMPLEMENTED (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            break;
+            case DataPilotFieldReferenceType::TOTAL_PERCENTAGE:
+            {
+                // Same as DataPilotFieldReferenceType::ROW_PERCENTAGE , but the grand 
+                // total for the result's data field is used. 
+                fprintf(stdout, "* TEST CODE NOT IMPLEMENTED (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            break;
+            case DataPilotFieldReferenceType::INDEX:
+            {
+                // The row and column totals and the grand total, following the same 
+                // rules as above, are used to calculate the following expression.
+                fprintf(stdout, "* TEST CODE NOT IMPLEMENTED (%ld, %ld)\n", cell.Row, cell.Column);
+                ++mnFailureCount;
+            }
+            break;
+            default:
+                fprintf(stdout, "* UNKNOWN REFERENCE TYPE (%ld, %ld)\n",
+                        cell.Row, cell.Column);
+                ++mnFailureCount;
+        }
+    }
+
 private:
     ResultTester(); // disabled
     Reference<XDataPilotTable2>     mxDPTab;
     RuntimeData                     maData;
-    vector<sal_Int32>               maDataFieldIds;
-    vector<sheet::GeneralFunction>  maFuncs;
+    vector<DataFieldSetting>        maDataFieldSettings;
     sal_Int16                       mnFailureCount;
 };
 
