2316dc8568615ad12fcb69e5af39bdf88c34c5ee
Petr Mladek <pmladek@suse.cz>
2009-02-27 19:00:52 +0000

    Udpate to allow diffs against files in the topdir
    * bin/sloppypatch.pl, patches/apply.pl.in: udpate to allow diffs
    against files in the topdir
    
    * patches/dev300/piece-reportdesign.diff:
    * patches/dev300/piece-swext.diff:
    * patches/dev300/piece-helpcontent2.diff:
    * patches/dev300/piece-build.diff:
    * patches/dev300/apply: update for ooo310-m2
    
    * patches/dev300/piece-postprocess.diff:
    * patches/dev300/apply: bin; already included in ooo310-m2
    
    * patches/dev300/apply: temporary disable piece-reportbuilder.diff;
    still not available in DEV300/OOO310 :-(
    
    * src/setup_native_packinfo-ooo310-m2.tar.bz2: added
    * src/setup_native_packinfo-ooo300-m4.tar.bz2: bin obsolete
    * bin/unpack: update setup_native_packinfo version
    
    * bin/piece/sys-setup.in: define VERSION variable
    * bin/piece/file-list-postprocess:
    * bin/piece/file-list-l10n: use $VERSION instead of hardcoded

--- bin/sloppypatch.pl
+++ bin/sloppypatch.pl
@@ -17,89 +17,174 @@ for (my $idx = 0; $idx < @ARGV; $idx++) {
     }
 }
 
-my $tmpfile, $fh;
+my $tmpfile;
+my $fh;
 ($fh,$tmpfile) = mkstemp("/tmp/sloopypatch-XXXXXX");
 
-sub analyze_path($)
+sub path_exists($)
 {
-    my $pf = shift;
+    my $path = shift;
     my $topdir = undef;
     my $subpath = undef;
+
+#    print "Checking path: $path\n";
     
-#    print "analyzing path: $pf\n";
-    $pf =~ s|\.\/+||;
-    if ( $pf =~ m/^([^\/]+)([^ \t]+)/ ) {
+    # skip initial ./ is needed
+    $path =~ s|\.\/+||;
+
+    if ( $path =~ m/^([^\/]+)([^ \t]+)/ ) {
 	$topdir = "$1";
 	$subpath = "$2";
 	chomp $subpath;
     }
     
-    return $topdir, $subpath;
+    # the relative paths starting with ".." must be invalid because we use
+    # the -p0 patches and the upper direcotry name is different with every milestone
+    if ( (defined $topdir && "$topdir" ne ".." && -d "$applydir/$topdir" ) || ( -f "$applydir/$path") ) {
+#	print "   passed $topdir,$path\n";
+	return $path;
+    } else {
+#	print "   not passed\n";
+	return undef;
+    }
 }
 
-my $applying = 0;
-my $firstline = undef;
-my $firstline_topdir = undef;
-my $firstline_subpath = undef;
+my $applying = undef;
 my $sections = 0;
+my $path1 = undef;
+my $path2 = undef;
+my @header_lines = ();
+my $header_unified = 0;
+my $header_found = 0;
+my $line_matched = 0;
 while (<STDIN>) {
     my $line = $_;
 
-    # unified context patches:
-    # 	first line:  --- path
-    #	second line: +++ path
-    # copied context patches:
-    # 	first line:  *** path
-    #	second line: --- path
-    # IMPORTANT: we need extra reqular expression for --- and *** to do not match lines like -**
-    if  ( ( ! defined $firstline ) &&
-          ( ( $line =~ m/^\*\*\* [ \t]*([^\s]+)/ ) ||
-	    ( $line =~ m/^--- [ \t]*([^\s]+)/ ) ) ) {
-	# found first line that defines the path of the patched file
-	($firstline_topdir, $firstline_subpath) = analyze_path("$1");
-	$firstline = $line;
-	next;
+    # same of unified context patch header:
+    # --- file.old	2009-02-26 20:04:16.000000000 +0100 
+    # +++ file		2009-02-26 20:04:41.000000000 +0100 
+    # @@ -1,26 +1,28 @@ 
+
+    # sample of copied context patch header:
+    # *** file.old 2009-02-26 20:04:16.000000000 +0100 
+    # --- file     2009-02-26 20:04:41.000000000 +0100 
+    # *************** 
+    # *** 1,26 **** 
+    
+    $line_matched = 0;
+    
+    if (! @header_lines) {
+	if ( $line =~ m/^--- [\s]*([^\s]+)/ ) {
+	    $path1 = "$1";
+	    $header_unified = 1;
+	    $line_matched = 1;
+	}
+	if ( $line =~ m/^\*\*\* [\s]*([^\s]+)/ ) {
+	    $path1 = "$1";
+	    $header_unified = 0;
+	    $line_matched = 1;
+	}	    
+	# hack to support unified diffs where the --- line is missing
+	if ( $line =~ m/^\+\+\+ [\s]*([^\s]+)/ ) {
+	    # only line 2
+	    $path2 = "$1";
+	    $header_unified = 1;
+	    $line_matched = 1;
+	    # fake line 1
+	    $path1 = $path2;
+	    push @header_lines, "";
+	}
+    } elsif (@header_lines == 1) {
+	if ($header_unified) {
+	    if ( $line =~ m/^\+\+\+ [\s]*([^\s]+)/ ) {
+		$path2 = "$1";
+		$line_matched = 1;
+	    }
+	} else {
+	    if ( $line =~ m/^--- [\s]*([^\s]+)/ ) {
+		$path2 = "$1";
+		$line_matched = 1;
+	    }
+	}
+    } elsif (@header_lines == 2) {
+	if ($header_unified) {
+	    if ( $line =~ m/^\@\@ -(\d+),(\d+) \s*\+\d+,\d+ \s*\@\@/ ) {
+		# new file looks like: @@ -0,0 +1,23 @@ 
+		$new_file=1 if  (($1 == 0) && ($2 == 0));
+		$line_matched = 1;
+		$header_found = 1;
+#		print "Unified header found\n";
+	    }
+	} else {
+	    if ( $line =~ m/^\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*/ ) {
+		$line_matched = 1;
+	    }
+	}
+    } elsif (@header_lines == 3) {
+	if (! $header_unified) {
+	    if ( $line =~ m/^\*\*\* \s*(\d+)(,\d+)? \s*\*\*\*\*/ ) {
+		# new file looks like: *** 0 ****
+		$new_file = 1 if ( ( $1 == 0 ) && ( ! defined $2 ) );
+		$line_matched = 1;
+		$header_found = 1;
+#		print "Context header found\n";
+	    }
+	}
     }
-
-    # there are diffs with just the +++ line => the first line is missing in this cases
-    if ( ( ( defined $firstline ) && ( $line =~ m/^--- [ \t]*([^\s]+)/ ) ) ||
-         ( $line =~ m/^\+\+\+ [ \t]*([^\s]+)/ ) ) {
-	# found second line that defines the path of the patched file
-	($topdir, $subpath) = analyze_path("$1");
-
+    
+    if (@header_lines && ! $line_matched ) {
+	# looked like header but it is not header in the end
+	if ( $applying ) {
+	    # print the saved lines if applying this piece
+	    foreach (@header_lines) {
+		print $fh $_;
+	    }
+	}
+	@header_lines = ();
+	# FIXME: should check the previous lines again???
+	#        or at least the last one; might be there extra ---, +++ lines without real hunk?
+	#        something like?
+	#        --- fileA
+	#        +++ fileA
+	# 	 --- fileB
+	# 	 +++ fileB
+	#        @@ -10,6 +10,7 @@
+	# well this will ve coverted by the hack for missing --- line, see above;
+    }
+    
+    if ( $header_found ) {
+	# accept new path without the "path_exists" check only in topdir
+	$applying = undef;
+	$applying = $path2 if ( $new_file && ( ! $path2 =~ m/^(\.\/+)?[^\/]+\/[^\/]+/ ) );
 	# need to check all ***, +++, --- paths; any of them might be correct
 	# the other path might be invalid, e.g. absolute path
-	# the relative paths starting with ".." must be invalid because we use
-	# the -p0 patches and the upper direcotry name is different with every milestone
-	if ( defined $topdir && "$topdir" ne ".." && -d "$applydir/$topdir" ) {
-#	    print "topdir: $topdir\n";
-	    $applying = 1;
-	} elsif ( defined $firstline_topdir && "$firstline_topdir" ne ".." && -d "$applydir/$firstline_topdir" ) {
-#	    print "firstline_topdir: $firstline_topdir\n";
-	    $applying = 1;
-	    $topdir = $firstline_topdir;
-	    $subpath = $firstline_subpath;
-	} else {
-	    $applying = 0;
-	}
-
-	if ($applying) {
+	$applying = path_exists( $path2 ) if ( ! $applying );
+	$applying = path_exists( $path1 ) if ( ! $applying );
+	
+	if ( $applying ) {
 	    $sections++;
-	    print STDERR "+ apply fragment for $topdir$subpath\n";
+	    print STDERR "+ apply fragment for $applying\n";
+	    # print the saved header lines
+	    foreach (@header_lines) {
+		print $fh $_;
+	    }
 	}
-    
-    }
+	@header_lines = ();
+	$new_file = 0;
+	$header_found = 0;
+	$line_matched = 0;
+    }	    
 
-    if (defined $firstline) {
-	# either it is the real first line that defines the file path
-	# or it might be the hunk definition in the copied context patches
-	# we should print it in both cases
-	$line = $firstline . $line;
-	$firstline = undef;
-    }
-
-    if ($applying) {
-	print $fh $line;
+    if ($line_matched) {
+	# save the header line
+	push @header_lines, $line;
+	$line_matched = 0;
+#	print "Matched header line $#header_lines\n";
+    } else {
+	# no header line => print it if applying this piece
+	if ( $applying ) {
+	    print $fh $line;
+	}
     }
 }
 $fh->close;
